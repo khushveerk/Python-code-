5c500aa32eb69e5f26a87ef52caa5edde309648a
khushveerk <khushveer.prof011@gmail.com>
2024-12-26 20:48:04 +0530

    Update practice2.py

--- practice2.py
+++ practice2.py
@@ -1,5 +1,40 @@
 
 #
+def occurances(a,b):
+  
+  newlist = []
+
+  
+  if b not in a:
+   
+    print(-1)
+ 
+  else:
+    
+    i=0
+   
+    while i<=len(a):
+     
+      c=a.find(b,i)
+     
+      if c== -1:
+      
+       break
+      
+      i = c+len(b)
+      
+      newlist.append(c)
+    
+    print(newlist)
+
+a=input("enter first string; ")
+
+b=input("enter second string; ")
+
+occurances(a,b)
+
+
+#
 name=input("enter a name")
 try:
     if name.isalpha():
